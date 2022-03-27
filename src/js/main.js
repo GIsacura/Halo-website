96c5b0aae784d5d988ece09538416f6e937f4026
Gustavo Isacura <i.tavo1512@gmail.com>
2022-03-27 00:51:12 -0400

    Agregando js a menu escritorio y mobile

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const moreOptions = document.querySelector("#bmore");
+const bShowMobileLinks = document.querySelector("#bmenu");
+const mobileMenu = document.querySelector(".links");
+const moreMenu = document.querySelector(".more .menu")
+
+bShowMobileLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.toggle("show"); // En esta linea con el toggle hacemos que si mobileMenu tiene la clase show se la quita y si no la tiene se la coloca
+});
+
+moreOptions.addEventListener("click", (e) => {
+  e.preventDefault();
+  moreMenu.classList.toggle("show");
+});
